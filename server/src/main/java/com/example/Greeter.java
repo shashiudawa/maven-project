1eb9d6441cc8d7cf188598bd52938c5f838fe9ce
shashiudawa <shashiudawa@gmail.com>
2017-05-17 00:33:51 -0400

    check style

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,6 +12,11 @@ public class Greeter {
 
   }
 
+/**
+ * @param someone the name of a person
+ * @return greeting strung
+ */
+
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
